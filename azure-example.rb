f54bda4916025e71a1a763cf9cd95858d89e260d
jsturtevant <jsturtevant@gmail.com>
2015-08-20 09:01:56 -0400

    removed key.  regenerated

--- azure-example.rb
+++ azure-example.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 require "redis"
 
 print "Hello!\n"
-redis = Redis.new(:host => "jstur.redis.cache.windows.net", :port => 6380, :db => 10, :password => "acidyHTxYc+jZCSeleV8pjUDEe78L2fFqpo4gZ0u5EI=", :use_ssl => true)
+redis = Redis.new(:host => "<your cache name>.redis.cache.windows.net", :port => 6380, :db => 10, :password => "<KEY>", :use_ssl => true)
 
 redis.set "sample", "Goodbye"
 
